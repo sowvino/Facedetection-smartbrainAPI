3965f9c259a4e736dd98b07c82a600fb4e2869e8
sowvino <106254533+sowvino@users.noreply.github.com>
2022-12-13 20:26:47 +0100

    Initial commit

---
+++ controllers/image.js
@@ -0,0 +1,49 @@
+const handleApicall = (req, res) => {
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": 'sowmi123',
+      "app_id": 'qa4p7rj46qrg'
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": req.body.input,
+          }
+        }
+      }
+    ]
+  });
+
+  // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+  // this will default to the latest version_id
+  fetch(`https://api.clarifai.com/v2/models/f76196b43bbd45c99b4f3cd8e8b40a8a/outputs`,
+    {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        // eslint-disable-next-line no-useless-concat
+        'Authorization': 'Key ' + '3d94fd1edbbd423fafae6335fe1cb1e6'
+      },
+      body: raw
+    })
+    .then(response => response.text()).then(data => res.json(data))
+}
+
+
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entry', 1)
+    .returning('entry')
+    .then(entry => {
+      res.json(entry[0].entry);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+  handleImage,
+  handleApicall
+};
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        port: 5432,
+        user: 'postgres',
+        password: 'Achu@2019',
+        database: 'smart-brain'
+    }
+});
+
+//db.select('*').from('users').then(data => console.log(data));
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+//signing in the form
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
+
+
+//registration form 
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+
+//getting the user profile once it is signed in using id params
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) });
+
+///counter for no. of times user enters the profile image entry is incremented
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.post('/imageurl', (req, res) => { image.handleApicall(req, res) });
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('app is working');
+});
+
+
+console.log(process.env.PORT);
